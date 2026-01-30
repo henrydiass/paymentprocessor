fa1f6520049f81d5ef1f897ac94b327fc129fca7
Henry <Henry_Duas@hotmail.com>
2026-01-29 21:59:22 -0300

    fix: enabled async and added @service to NotificationService

--- src/main/java/com/paymentprocessor/domain/services/NotificationService.java
+++ src/main/java/com/paymentprocessor/domain/services/NotificationService.java
@@ -1,4 +1,4 @@
-package com.paymentprocessor.services;
+package com.paymentprocessor.domain.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
